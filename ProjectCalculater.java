adf7978925fc875859e53ec48abc8cfe77bbcab6
jirayuwongtom <lovelava600@gmail.com>
2026-01-29 13:48:08 +0700

    add Democalculater

---
+++ ProjectCalculater.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class ProjectCalculater {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter X : ");
+        int x = input.nextInt();
+
+        System.out.print("Enter operator (+ - * / %) : ");
+        String op = input.next();
+
+        System.out.print("Enter Y : ");
+        int y = input.nextInt();
+        
+        if (op.equals("+")) {
+            System.out.print("Result = " + (x + y));
+        }else if (op.equals("-")) {
+            System.out.print("Result = " + (x - y));
+        }else if (op.equals("*")) {
+            System.out.print("Result = " + (x * y));
+        }else if (op.equals("/")) {
+            if (y != 0) {
+                System.out.print("Result = " + (x / y));
+            }else{
+                System.out.println("Error: Cannot divide by zero");
+            }
+        }else if (op.equals("%")) {
+            System.out.print("Result = " + (x % y));
+        }else{
+            System.out.println("Invalid operator");
+        }
+        input.close();
+    }
+}
