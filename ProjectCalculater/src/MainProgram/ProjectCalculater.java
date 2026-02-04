01fa3c66e0d21ba94cb1123f9fe72c424f0bb044
jirayuwongtom <lovelava600@gmail.com>
2026-02-04 17:41:42 +0700

    add feature&configmain

--- ProjectCalculater/src/MainProgram/ProjectCalculater.java
+++ ProjectCalculater/src/MainProgram/ProjectCalculater.java
@@ -1,31 +1,103 @@
 package MainProgram;
 import java.util.Scanner;
 public class ProjectCalculater {
-    static class Percent {
-        public double calculate(double x, double y) { return (x * y) / 100; }
+    static class Calculator {
+        public double calculate(double x, double y) { 
+            return x;
+        } 
+    }
+    static class Add extends Calculator { 
+        public double calculate(double x, double y) { 
+            return x + y; 
+        } 
+    }
+    static class Subtract extends Calculator { 
+        public double calculate(double x, double y) { 
+            return x - y; 
+        } 
+    }
+    static class Multiply extends Calculator {
+        public double calculate(double x, double y) {
+            return x * y;
+        }
+    }
+    static class Divide extends Calculator {
+        public double calculate(double x, double y) {
+            if (y == 0) {
+                System.out.println("It cannot be divided by 0.");
+                return 0;
+            }
+            return x / y;
+        }
+    }
+    static class Percent extends Calculator {
+        public double calculate(double x, double y) { 
+            return (x * y) / 100; 
+        }
     }
 
     public static void main(String[] args) {
-        System.out.print("Welcome to Simple Calculator!");
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter number: ");
-        String input = scan.nextLine();
-        input = input.replace(" ", "");
-        int pos = input.indexOf('%');
-        if (pos == -1) {
-            System.out.println("Incorrect format: It should be... a%b");
-            scan.close();
-            return;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        String Sinput = input.nextLine(); 
+        double[] numbers = new double[100]; 
+        char[] operators = new char[100];
+        int numCount = 0;
+        int opCount = 0;
+        String tempNum = "";
+        for (int i = 0; i < Sinput.length(); i++) {
+            char c = Sinput.charAt(i);
+            if (c != '+' && c != '-' && c != '*' && c != '/' && c != '%') {
+                tempNum += c;
+            }
+            if (c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || i == Sinput.length() - 1) {
+                if(tempNum.isEmpty()) continue;
+                numbers[numCount++] = Double.valueOf(tempNum);
+                tempNum = ""; 
+                if (i < Sinput.length() - 1) {
+                    operators[opCount++] = c;
+                }
+            }
         }
-        try {
-            double left = Double.parseDouble(input.substring(0, pos));
-            double right = Double.parseDouble(input.substring(pos+1));
-            Percent p = new Percent();
-            double result = p.calculate(left, right);
-            System.out.println("Calculate: " + left + " % " + right + " = " + result);
-            System.out.println("Result = " + result);
-        } catch (NumberFormatException e) {
+        for (int i = 0; i < opCount; i++) {
+            char op = operators[i];
+            if (op == '*' || op == '/' || op == '%') {
+                Calculator calc = null;
+                if (op == '*') calc = new Multiply();
+                else if (op == '/') calc = new Divide();
+                else if (op == '%') calc = new Percent();
+                double val1 = numbers[i];
+                double val2 = numbers[i+1];
+                double result = calc.calculate(val1, val2);
+                System.out.println("    Process: " + val1 + " " + op + " " + val2 + " = " + result);
+                numbers[i] = result;
+                for (int j = i + 1; j < numCount - 1; j++) {
+                    numbers[j] = numbers[j + 1];
+                }
+                for (int j = i; j < opCount - 1; j++) {
+                    operators[j] = operators[j + 1];
+                }
+                numCount--;
+                opCount--;
+                i--;
+            }
         }
-        scan.close();
+        double finalResult = numbers[0];
+        for (int i = 0; i < opCount; i++) {
+            char op = operators[i];
+            double nextNumber = numbers[i+1];
+            double oldResult = finalResult; 
+            Calculator calc = null;
+            if (op == '+') calc = new Add();
+            else if (op == '-') calc = new Subtract();
+            
+            if (calc != null) {
+                finalResult = calc.calculate(finalResult, nextNumber);
+                System.out.println("    Process: " + oldResult + " " + op + " " + nextNumber + " = " + finalResult);
+            }
+        }
+        System.out.println("-----------------------------------");
+        System.out.println("Output : " + finalResult);
+        input.close();
     }
 }
